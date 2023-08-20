a29d2471b1d761a1e31bb6f40dec68f41ac5113a
D-Mastrocola <mastrodomcola@gmail.com>
2023-08-20 19:27:12 -0400

    initial commits

---
+++ app/src/components/About/About.jsx
@@ -0,0 +1,55 @@
+import Header from "../Header/Header";
+
+let About = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus
+          vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt
+          ornare. Gravida arcu ac tortor dignissim. Pharetra diam sit amet nisl.
+          Nullam vehicula ipsum a arcu cursus. Fermentum et sollicitudin ac
+          orci. Cursus mattis molestie a iaculis at. Scelerisque varius morbi
+          enim nunc faucibus a pellentesque sit amet. Nulla aliquet porttitor
+          lacus luctus accumsan tortor posuere ac ut. Enim blandit volutpat
+          maecenas volutpat blandit aliquam etiam. Nisi est sit amet facilisis
+          magna etiam tempor orci eu. Amet nisl purus in mollis nunc. Pretium
+          lectus quam id leo in vitae turpis massa sed. Dignissim suspendisse in
+          est ante in. Sagittis purus sit amet volutpat consequat. Quam
+          vulputate dignissim suspendisse in. Tristique senectus et netus et.
+          Eget egestas purus viverra accumsan in nisl nisi scelerisque. Arcu
+          dictum varius duis at consectetur lorem donec massa sapien.
+        </p>
+        <p>
+          Aliquam purus sit amet luctus venenatis lectus magna. Mattis
+          ullamcorper velit sed ullamcorper morbi tincidunt ornare. Eget nunc
+          scelerisque viverra mauris in aliquam sem. Dui accumsan sit amet nulla
+          facilisi morbi tempus. Phasellus egestas tellus rutrum tellus
+          pellentesque eu tincidunt tortor. Orci dapibus ultrices in iaculis
+          nunc. Non consectetur a erat nam at lectus. Enim nulla aliquet
+          porttitor lacus. Sit amet purus gravida quis blandit. Tincidunt vitae
+          semper quis lectus nulla at volutpat. Magna etiam tempor orci eu
+          lobortis. Id interdum velit laoreet id. Quam quisque id diam vel quam
+          elementum pulvinar etiam non. Porttitor leo a diam sollicitudin.
+          Ornare quam viverra orci sagittis. Massa eget egestas purus viverra
+          accumsan in nisl nisi.
+        </p>
+        <p>
+          Nulla aliquet enim tortor at auctor urna nunc id cursus. Facilisis
+          mauris sit amet massa. Nulla at volutpat diam ut venenatis tellus in
+          metus. Sed blandit libero volutpat sed cras ornare arcu. Vitae semper
+          quis lectus nulla at volutpat diam ut venenatis. Mauris vitae
+          ultricies leo integer. Rhoncus aenean vel elit scelerisque mauris
+          pellentesque pulvinar pellentesque habitant. Cras semper auctor neque
+          vitae tempus quam pellentesque nec. Habitant morbi tristique senectus
+          et netus. Neque aliquam vestibulum morbi blandit cursus risus. Non
+          quam lacus suspendisse faucibus.
+        </p>
+      </main>
+    </>
+  );
+};
+export default About;
